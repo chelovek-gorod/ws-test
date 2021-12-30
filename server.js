df124ab2c865612ffb804b5f2fe12cd50b62a607
Marsel <47145234+chelovek-gorod@users.noreply.github.com>
2021-12-30 16:35:20 +0300

    update

--- server.js
+++ server.js
@@ -1,56 +1,111 @@
 const WebSocket = require('ws');
-const usedPort = process.env.PORT || 8080;
-const wsServer = new WebSocket.Server({port: usedPort});
-wsServer.on('connection', onConnect);
+
+const usedPort = process.env.PORT || 9000;
+const socketServer = new WebSocket.Server({ port: usedPort });
+socketServer.on('connection', onConnect);
 
 class Client {
-  constructor(id, name) {
+  constructor(id, nickName, avatar) {
     this.id = id;
-    this.name = name;
+    this.nickName = nickName;
+    this.avatar = avatar;
   }
 }
+let clientsArr = [];
 
-let clients = [];
+const maxMessagesOnServer = 100;
+let messagesArr = new Array(maxMessagesOnServer);
+/*
+{ 
+  senderNickName: string,
+  senderAvatar: id,
+  message: object{
+    type: message / answer,
+    data: text / sticker
+  }
+}
+*/
 
-function onConnect(wsClient) {
-  console.log('Новое подключение');
-  wsClient.on('message', function(message) {
-    const jsonMessage = JSON.parse(message);
-    switch(jsonMessage.action) {
-      case 'REG' :
-        let userExist = clients.find(object => {
-          if (object.name === jsonMessage.data) return true;
-        });
-        if (userExist) {
-          wsClient.send(JSON.stringify({action: 'REG', data: false}));
-        } else {
-          let client = new Client(wsClient, jsonMessage.data)
-          clients.push(client);
-          console.log('Зарегился: ' + jsonMessage.data);
-          wsClient.send(JSON.stringify({action: 'REG', data: true}));
-        }
-        break;
-      case 'MSG' :
-        clients.forEach(client => {
-          client.id.send(JSON.stringify({
-            action: 'MSG',
-            data: {user: jsonMessage.data.user, message: jsonMessage.data.message}
-          }));
-        });
-        break;
-      default : console.log('-- пришол кривой action --');
+function onConnect(socketClient) {
+  console.log('get new connection');
+
+  socketClient.on('message', function (message) {
+    let { action, data } = JSON.parse(message);
+    switch (action) {
+      case 'registration' : getRegistrationRequest(socketClient, data); break;
+      case 'onConnect' : getOnConnectRequest(socketClient, data); break;
+      case 'newMessage' : getNewMessageRequest(socketClient,data); break;
+      default : getWrongActionInRequest(action, data);
     }
-  })
-  wsClient.on('close', function() {
-    clients = clients.filter(client => client !== wsClient);
-    console.log('-- отключение --');
-    clients.forEach(client => {
+  });
+
+  socketClient.on('close', function () {
+    clientsArr = clientsArr.filter(client => client !== socketClient);
+
+    console.log('user disconnect');
+
+    clientsArr.forEach(client => {
       client.id.send(JSON.stringify({
-        action: 'MSG',
-        data: {user: "<!>", message: client.name + " отключился..."}
+        action: 'disconnectionUser',
+        data: {
+          nickName: client.nickName,
+          avatar: client.avatar
+        }
       }));
     });
-  })
+  });
+
+}
+console.log(`server start on port ${usedPort}`);
+
+function getOnConnectRequest(socketClient, data) {
+  socketClient.send(JSON.stringify({
+    action: 'onConnect',
+    data: { clientSendTime: data, serverSendTime: Date.now() }
+  }));
 }
 
-console.log('Сервер запущен на порту ' + usedPort);
+function getRegistrationRequest(socketClient, data) {
+  let userAvatarExist = clientsArr.find(object => {
+    if (object.avatar === data.avatar) return true;
+  });
+  let userNickNameExist = clientsArr.find(object => {
+    if (object.nickName === data.nickName) return true;
+  });
+
+  let registrationIs = (userAvatarExist || userNickNameExist) ? false : true;
+
+  if (registrationIs) {
+    clientsArr.forEach(client => {
+      client.id.send(JSON.stringify({
+        action: 'newUser',
+        data: {
+          nickName: data.nickName,
+          avatar: data.avatar
+        }
+      }));
+    });
+
+    let client = new Client(socketClient, data.nickName, data.avatar);
+    clientsArr.push(client);
+
+  }
+
+  socketClient.send(JSON.stringify({
+    action: 'registration',
+    data: {
+      registrationIs: registrationIs,
+      userAvatarExist : userAvatarExist,
+      userNickNameExist : userNickNameExist
+    }
+  }));
+
+}
+
+function getNewMessageRequest(socketClient, data) {
+
+}
+
+function getWrongActionInRequest(action, data) {
+
+}
